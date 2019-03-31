c9953781db0e6a24c6182bf466605a75cb456ee9
Maxim Schuwalow <mschuwalow@agile-im.de>
2019-03-31 22:47:55 +0200

    add doobie integration

--- app/src/main/scala/com/schuwalow/zio/todo/domain.scala
+++ app/src/main/scala/com/schuwalow/zio/todo/domain.scala
@@ -1,41 +1,44 @@
 package com.schuwalow.zio.todo
 
-final case class TodoItem(
-  id: Long,
+final case class TodoId(value: Long) extends AnyVal
+
+final case class TodoPayload(
   title: String,
   completed: Boolean,
-  order: Option[Int]) {
+  order: Option[Int]
+)
+
+final case class TodoItem(
+  id: TodoId,
+  item: TodoPayload
+) {
 
   def update(form: TodoItemPatchForm): TodoItem =
-    this.copy(title       = form.title.getOrElse(title),
-              completed   = form.completed.getOrElse(completed),
-              order       = form.order.orElse(order))
-
-}
-
-final case class TodoItemWithUri(
-  url: String, id: Long,
-  title: String,
-  completed: Boolean,
-  order: Option[Int])
-
-object TodoItemWithUri {
-
-  def apply(basePath: String, payload: TodoItem): TodoItemWithUri =
-    TodoItemWithUri(s"$basePath/${payload.id}", payload.id, payload.title, payload.completed, payload.order)
-
+    this.copy(
+      id    = this.id,
+      item  = item.copy(
+                title       = form.title.getOrElse(item.title),
+                completed   = form.completed.getOrElse(item.completed),
+                order       = form.order.orElse(item.order)
+              )
+    )
 }
 
 final case class TodoItemPostForm(
   title: String,
-  order: Option[Int] = None) {
+  order: Option[Int] = None
+) {
 
-  def asTodoItem(id: Long): TodoItem =
-    TodoItem(id, title, false, order)
+  def asTodoItem(id: TodoId): TodoItem =
+    TodoItem(id, this.asTodoPayload)
+
+  def asTodoPayload: TodoPayload =
+    TodoPayload(title, false, order)
 
 }
 
 final case class TodoItemPatchForm(
   title: Option[String] = None,
   completed: Option[Boolean] = None,
-  order: Option[Int] = None)
+  order: Option[Int] = None
+)
--- app/src/main/scala/com/schuwalow/zio/todo/http/TodoService.scala
+++ app/src/main/scala/com/schuwalow/zio/todo/http/TodoService.scala
@@ -1,7 +1,7 @@
 package com.schuwalow.zio.todo.http
 
+import com.schuwalow.zio.todo._
 import com.schuwalow.zio.todo.repository._
-import com.schuwalow.zio.todo.{TodoItemPostForm, TodoItemPatchForm, TodoItemWithUri}
 import io.circe.generic.auto._
 import io.circe.{Decoder, Encoder}
 import org.http4s._
@@ -11,6 +11,7 @@ import scalaz.zio.TaskR
 import scalaz.zio.interop.catz._
 
 final case class TodoService[R <: TodoRepository](rootUri: String) {
+  import TodoService._
 
   type TodoTask[A] = TaskR[R, A]
 
@@ -20,37 +21,59 @@ final case class TodoService[R <: TodoRepository](rootUri: String) {
   val dsl: Http4sDsl[TodoTask] = Http4sDsl[TodoTask]
   import dsl._
 
-  def service: HttpRoutes[TodoTask] = HttpRoutes.of[TodoTask] {
+  def service: HttpRoutes[TodoTask] = {
 
-    case GET -> Root =>
-      Ok(getAll.map(_.map(TodoItemWithUri(rootUri, _))))
+    HttpRoutes.of[TodoTask] {
 
-    case GET -> Root / LongVar(id) =>
-      for {
-        todo     <- getById(id)
-        response <- todo.fold(NotFound())(x => Ok(TodoItemWithUri(rootUri, x)))
-      } yield response
+      case GET -> Root =>
+        Ok(getAll.map(_.map(TodoItemWithUri(rootUri, _))))
 
-    case req @ POST -> Root =>
-      req.decode[TodoItemPostForm] { todoItemForm =>
-        Created(create(todoItemForm).map(TodoItemWithUri(rootUri, _)))
-      }
-
-    case DELETE -> Root / LongVar(id) =>
-      for {
-        item     <- getById(id)
-        result   <- item.map(x => delete(x.id)).fold(NotFound())(Ok(_))
-      } yield result
-
-    case DELETE -> Root =>
-      deleteAll *> Ok()
-
-    case req @ PATCH -> Root / LongVar(id) =>
-      req.decode[TodoItemPatchForm] { updateForm =>
+      case GET -> Root / LongVar(id) =>
         for {
-          update   <- update(id, updateForm)
-          response <- update.fold(NotFound())(x => Ok(TodoItemWithUri(rootUri, x)))
+          todo     <- getById(TodoId(id))
+          response <- todo.fold(NotFound())(x => Ok(TodoItemWithUri(rootUri, x)))
         } yield response
-      }
+
+      case req @ POST -> Root =>
+        req.decode[TodoItemPostForm] { todoItemForm =>
+          Created(create(todoItemForm).map(TodoItemWithUri(rootUri, _)))
+        }
+
+      case DELETE -> Root / LongVar(id) =>
+        for {
+          item     <- getById(TodoId(id))
+          result   <- item.map(x => delete(x.id)).fold(NotFound())(Ok(_))
+        } yield result
+
+      case DELETE -> Root =>
+        deleteAll *> Ok()
+
+      case req @ PATCH -> Root / LongVar(id) =>
+        req.decode[TodoItemPatchForm] { updateForm =>
+          for {
+            update   <- update(TodoId(id), updateForm)
+            response <- update.fold(NotFound())(x => Ok(TodoItemWithUri(rootUri, x)))
+          } yield response
+        }
+    }
   }
+}
+
+object TodoService {
+
+  final case class TodoItemWithUri(
+    id: Long,
+    url: String,
+    title: String,
+    completed: Boolean,
+    order: Option[Int]
+  )
+
+  object TodoItemWithUri {
+
+    def apply(basePath: String, todoItem: TodoItem): TodoItemWithUri =
+      TodoItemWithUri(todoItem.id.value, s"$basePath/${todoItem.id.value}", todoItem.item.title, todoItem.item.completed, todoItem.item.order)
+
+  }
+
 }
