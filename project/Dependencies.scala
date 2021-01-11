880926271d29bef52f90b54268539019c5dc5433
Maxim Schuwalow <16665913+mschuwalow@users.noreply.github.com>
2021-01-11 19:02:31 +0100

    Update doobie-core, doobie-h2, ... to 0.10.0 (#110)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val circe      = "0.13.0"
-    val doobie     = "0.9.4"
+    val doobie     = "0.10.0"
     val http4s     = "0.21.15"
     val log4j      = "2.14.0"
     val zio        = "1.0.3"
