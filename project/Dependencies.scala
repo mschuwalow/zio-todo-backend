65eada8cf85981fb096574bc845e2e04f6887526
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2021-03-12 10:18:38 +0000

    Update zio, zio-test, zio-test-sbt to 1.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val doobie     = "0.10.0"
     val http4s     = "0.21.20"
     val log4j      = "2.14.0"
-    val zio        = "1.0.4-2"
+    val zio        = "1.0.5"
     val zioLogging = "0.5.7"
   }
   import Versions._
