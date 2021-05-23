1fccf42e44ca0a37eeabd17c2c8071a561e97494
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2021-05-23 00:40:21 +0000

    Update zio, zio-test, zio-test-sbt to 1.0.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val log4j            = "2.14.1"
     val organizeImports  = "0.5.0"
     val pureConfig       = "0.15.0"
-    val zio              = "1.0.7"
+    val zio              = "1.0.8"
     val zioInteropCats   = "2.4.1.0"
     val zioLogging       = "0.5.8"
   }
