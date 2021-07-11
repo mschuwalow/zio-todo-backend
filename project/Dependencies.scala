43e5d47f2e6cf79335fb5b5b4b155ff24899b946
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2021-07-11 01:12:27 +0000

    Update zio-interop-cats to 2.5.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val organizeImports  = "0.5.0"
     val pureConfig       = "0.16.0"
     val zio              = "1.0.7"
-    val zioInteropCats   = "2.4.1.0"
+    val zioInteropCats   = "2.5.1.0"
     val zioLogging       = "0.5.11"
   }
   import Versions._
