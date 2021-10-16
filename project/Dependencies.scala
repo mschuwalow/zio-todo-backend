b512408bb09a00997f12d70ddfea58711b82fd43
Maxim Schuwalow <16665913+mschuwalow@users.noreply.github.com>
2021-10-16 11:57:37 +0200

    Update kind-projector to 0.13.2 (#214)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val h2               = "1.4.200"
     val http4s           = "0.21.26"
     val jawn             = "1.0.1"
-    val kindProjector    = "0.13.1"
+    val kindProjector    = "0.13.2"
     val log4j            = "2.14.1"
     val organizeImports  = "0.5.0"
     val pureConfig       = "0.17.0"
