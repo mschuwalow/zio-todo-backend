67d216fd78951cf7a251b4695bc56041f76cd702
Scala Steward <me@scala-steward.org>
2021-11-23 22:41:58 +0100

    Update http4s-blaze-server, http4s-circe, ... to 0.21.31

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val doobie           = "0.13.4"
     val flyway           = "8.0.4"
     val h2               = "1.4.200"
-    val http4s           = "0.21.26"
+    val http4s           = "0.21.31"
     val jawn             = "1.0.1"
     val kindProjector    = "0.13.2"
     val log4j            = "2.14.1"
