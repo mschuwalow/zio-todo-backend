ec9af37ce0f7b560e714b47dac1773cc42e751dc
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2021-12-18 00:15:36 +0000

    Update pureconfig to 0.17.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val kindProjector    = "0.13.2"
     val log4j            = "2.15.0"
     val organizeImports  = "0.6.0"
-    val pureConfig       = "0.17.0"
+    val pureConfig       = "0.17.1"
     val zio              = "1.0.13"
     val zioInteropCats   = "2.5.1.0"
     val zioLogging       = "0.5.13"
