06fc4b82fe2a187a20065bc41db2a39599612309
Maxim Schuwalow <16665913+mschuwalow@users.noreply.github.com>
2022-05-07 19:40:48 +0200

    Update flyway-core to 8.5.10 (#306)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val betterMonadicFor = "0.3.1"
     val circe            = "0.14.1"
     val doobie           = "0.13.4"
-    val flyway           = "8.0.5"
+    val flyway           = "8.5.10"
     val h2               = "2.1.212"
     val http4s           = "0.21.26"
     val jawn             = "1.0.1"
