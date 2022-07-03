fd75e9d535785e024e5654aff431f5d8494e2cb9
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2022-07-03 00:30:35 +0000

    Update http4s-blaze-server, http4s-circe, ... to 0.21.33

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val doobie           = "0.13.4"
     val flyway           = "8.5.13"
     val h2               = "2.1.214"
-    val http4s           = "0.21.26"
+    val http4s           = "0.21.33"
     val jawn             = "1.3.2"
     val kindProjector    = "0.13.2"
     val log4j            = "2.17.2"
