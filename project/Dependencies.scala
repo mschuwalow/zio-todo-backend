14b5eaf171455a2531f1354921ef114c7188d150
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2025-05-04 00:55:44 +0000

    Update flyway-core to 11.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val betterMonadicFor = "0.3.1"
     val circe            = "0.14.13"
     val doobie           = "1.0.0-RC9"
-    val flyway           = "11.1.1"
+    val flyway           = "11.8.0"
     val h2               = "2.3.232"
     val http4s           = "0.23.30"
     val blaze            = "0.23.17"
