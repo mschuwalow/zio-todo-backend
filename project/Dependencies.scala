5eade636a3ba0e6dfd3073af9a992ff6470b334b
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2026-08-23 17:55:18 +0000

    Update http4s-circe, http4s-dsl to 0.23.36

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val doobie           = "1.0.0-RC10"
     val flyway           = "11.10.3"
     val h2               = "2.3.232"
-    val http4s           = "0.23.30"
+    val http4s           = "0.23.36"
     val blaze            = "0.23.17"
     val kindProjector    = "0.13.4"
     val log4j            = "2.25.5"
