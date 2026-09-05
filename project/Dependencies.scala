650226e96865d41234f9be374c916ccf77d48d70
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2026-09-05 01:48:03 +0000

    Update h2 to 2.5.250

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val circe            = "0.14.16"
     val doobie           = "1.0.0-RC10"
     val flyway           = "11.10.3"
-    val h2               = "2.3.232"
+    val h2               = "2.5.250"
     val http4s           = "0.23.36"
     val blaze            = "0.23.18"
     val kindProjector    = "0.13.4"
